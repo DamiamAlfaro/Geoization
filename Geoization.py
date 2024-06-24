9bd025b7b365f7672268716ab6fb63f5789d07b3
Damiam <149447958+DamiamAlfaro@users.noreply.github.com>
2024-06-23 21:31:22 -0700

    Create Geoization.py

---
+++ Geoization.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+fileLocation = "FileLocationInComputer"
+
+file = pd.read_csv(fileLocation)
+
+print(file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
